dc599a9fc16b3f930835c7c9a30e0b3796c4722b
Vicky-peace <victorpeace47@gmail.com>
2023-09-05 21:31:42 +0300

    New Task

--- jsFundamentals/controllStructure.js
+++ jsFundamentals/controllStructure.js
@@ -118,8 +118,32 @@ const tips = [];
 const totals = [];
 
 const calcTip = bill => bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill ;
-for(let i=0 ; i <= bills.length ; i++){
-   console.log(calcTip(i)) ;
-   tips.push(calcTip(i));
+
+
+for(let i=0 ; i < bills.length ; i++){
+
+  const tip = calcTip(bills[i]);
+   
+
+   tips.push(tip);
+   totals.push(tip + bills[i]);
+
+   
     
 }
+console.log(bills,tips,totals);
+
+
+
+const calcAverage = function(arr){
+  let sum = 0;
+  for(let i = 0 ; i < arr.length; i++){
+    // sum = sum + arr[i]
+    sum += arr[i];
+  }
+  console.log(sum);
+  return sum /arr.length
+}
+console.log(calcAverage([2,3,7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
